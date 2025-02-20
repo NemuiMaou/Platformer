ba60496444cf6994aaf7dc1507a9bfcab6c082cb
NemuiMaou <djromar96@gmail.com>
2025-02-19 21:08:37 -0800

    Initial commit

---
+++ Assets/Platformer/Scripts/CameraPan.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+    public float speed = 5f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("CameraPan");
+        transform.position += new Vector3(direction, 0f, 0f) * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Platformer/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI coinCountText;
+    public TextMeshProUGUI totalScoreText;
+    private int score;
+    private int coinCount;
+    private int coinScoreIncrease = 100;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        int timeLeft = 300 - (int)Time.time;
+        timerText.text = $"Time: \n {timeLeft}";
+
+        if (Input.GetMouseButtonDown(0))  
+        {
+            RaycastHit hit;
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition); 
+
+            if (Physics.Raycast(myRay, out hit))
+            {
+                GameObject myObject = hit.collider.gameObject;
+                if (myObject.CompareTag("Brick"))
+                {
+                    Destroy(myObject);
+                }
+
+                if (myObject.CompareTag("QuestionBox"))
+                {
+                    coinCount++;
+                    ScoreIncrease(coinScoreIncrease);
+                    String coinFormat = coinCount.ToString("00");
+                    coinCountText.text = "x" + coinFormat;
+                }
+            }
+        }
+    }
+
+    public void ScoreIncrease(int points)
+    {
+        score += points;
+        String scoreFormat = score.ToString("000000");
+        totalScoreText.text = "Mario\n" + scoreFormat;
+    }
+}
